b6eecfb6b3951b28ca9579165a8e5d9127ecffde
Soy Victoria <victoriasulamitgarciaromo4087@gmail.com>
2025-11-20 14:18:04 -0600

    Mis cambios locales

--- ui.R
+++ ui.R
@@ -1,12 +1,14 @@
 ui <- fluidPage(
-  titlePanel("Factorial Design Experiment (2^2)"),
+  titlePanel("Factorial Diseño de Experimento (2^k)"),
   sidebarLayout(
+
     sidebarPanel(
       numericInput("rep", "Number of Replications:", 2, min = 1, max = 10),
       selectInput("factorA", "Factor A Levels:", choices = c("Low" = -1, "High" = 1)),
       selectInput("factorB", "Factor B Levels:", choices = c("Low" = -1, "High" = 1)),
       actionButton("run", "Run Experiment")
     ),
+
     mainPanel(
       tabsetPanel(
         tabPanel("Design", tableOutput("designTable")),
